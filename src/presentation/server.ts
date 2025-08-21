ae1dbb2ea47d3e70cdf7acb154b32e894dc23e7e
RichardRodri26 <richardmanuel811@gmail.com>
2025-08-20 22:01:31 -0500

    feat: enhance logging and configuration management with dotenv and env-var

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource()
+)
 
 export class Server {
   public static start() {
@@ -10,6 +16,7 @@ export class Server {
       const url = "https://www.google.com"
 
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`${url} is ok`),
         (error) => console.log(`Check service error: ${error}`)
       ).execute(url)
